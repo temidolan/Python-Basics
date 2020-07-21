e15a04dc61f1464ec071033691bd09056311c12d
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-07-21 00:23:31 -0400

    final submission

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -3,6 +3,7 @@ import os
 import csv
 #set the path for the file
 csvpath= os.path.join("Resources/election_data.csv")
+exportpath=os.path.join("Resources/Analysis/Election_result.txt")
 #setting my variables
 Total_votes=0
 Candidates_List=[]
@@ -28,6 +29,8 @@ with open(csvpath) as Election_data:
             candidates_dict[i[2]]=candidates_dict[i[2]] + 1
             # else if the candidate is already in the candiate list, 
             #just add one to their value
+Output1= (f"Election Results\n" + f"-------------------------\n" + f"Total Votes: {Total_votes}\n" + f"-------------------------\n")
+      
 winning=list(set(candidate))
 winning.sort()
 voters=[]
@@ -41,7 +44,7 @@ for candidate_name in candidates_dict:
     Percentage_list.append(Percentage_votes)
     #print(Percentage_list)
 winner=winning[voters.index(max(voters))]
-
+Election_Results=""
 count=0
 print("-------------------------")
 print("Election Results")
@@ -49,8 +52,16 @@ print("-------------------------")
 print(f"Total Votes: {Total_votes}")
 print("-------------------------")
 for i,x in candidates_dict.items():
-    print(f"{i}: {Percentage_list[count]}   ({x})")
+    print (f"{i}: {Percentage_list[count]}   ({x})")
+    Results = (f"{i}: {Percentage_list[count]}   ({x})\n")
     count+=1
+    Election_Results+=Results
 print("-------------------------")
 print("winner: ",winner)
 print("-------------------------")
+
+Output2=(f"-------------------------\n" + f"winner:  {winner}\n" + f"-------------------------\n")
+FinalAnalysis= (Output1 + Election_Results + Output2)
+with open(exportpath, "w",) as txt_file:
+        txt_file.write(FinalAnalysis)
+
