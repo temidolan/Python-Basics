6aeb2b6e457c8f6e200c2233a7c19adc39848c45
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-07-20 23:52:29 -0400

    second final sub

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -6,7 +6,7 @@ csvpath= os.path.join("Resources/election_data.csv")
 #setting my variables
 Total_votes=0
 Candidates_List=[]
-Percentage_votes=[]
+Percentage_list=[]
 Unique_candidate_list=[]
 candidates_dict = {}
 candidate=[]
@@ -28,7 +28,6 @@ with open(csvpath) as Election_data:
             candidates_dict[i[2]]=candidates_dict[i[2]] + 1
             # else if the candidate is already in the candiate list, 
             #just add one to their value
-#print(candidates_dict)
 winning=list(set(candidate))
 winning.sort()
 voters=[]
@@ -37,21 +36,21 @@ for i in winning:
         
 for candidate_name in candidates_dict:
     votes = candidates_dict.get(candidate_name)
-    Percentage_votes=(votes/Total_votes)*100
+    Percentage_votes="{0:.00%}".format(votes/Total_votes)
     #print(Percentage_votes)
+    Percentage_list.append(Percentage_votes)
+    #print(Percentage_list)
 winner=winning[voters.index(max(voters))]
+
+count=0
+print("-------------------------")
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {Total_votes}")
+print("-------------------------")
+for i,x in candidates_dict.items():
+    print(f"{i}: {Percentage_list[count]}   ({x})")
+    count+=1
+print("-------------------------")
 print("winner: ",winner)
-
-# #for candidate_name in candidates_dict:
-#     #print(candidate_name + )
-
-# #exportingprintstatement
-# #analysis =print(
-# #f"Election Results\n"
-# #f"--------------------\n"
-# #f"Total Votes: {total_votes}\n"
-# #f"{}\n"
-# #f"-------------------------\n"
-# f"winner:{winner}\n"
-# f"------------------------"
-# )
+print("-------------------------")
